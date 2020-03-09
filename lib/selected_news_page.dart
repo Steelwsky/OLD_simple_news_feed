7bdd6df9d8f4c7063294090bc42efa62df5fea79
steelwsky <zero.rise@yandex.ru>
2020-03-09 18:35:20 +1000

    added button for url launcher.

--- lib/selected_news_page.dart
+++ lib/selected_news_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:simplenewsfeed/viewed.dart';
 import 'package:webfeed/webfeed.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SelectedNewsPage extends StatelessWidget {
   SelectedNewsPage({Key key, this.rssItem, this.viewedItem}) : super(key: key);
@@ -27,6 +28,36 @@ class SelectedNewsPage extends StatelessWidget {
                           : viewedItem.content,
                       style: TextStyle(fontSize: 16),
                     ),
+                    Container(
+                      height: 50,
+                      width: 174,
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 16.0),
+                          child: InkWell(
+                            onTap: () async {
+                              final url = rssItem != null
+                                  ? rssItem.link
+                                  : viewedItem.link;
+                              if (await canLaunch(url)) {
+                                await launch(url);
+                              } else {
+                                throw 'Could not launch $url';
+                              }
+                            },
+                            child: Row(
+                              children: <Widget>[
+                                Text(
+                                  'Continue reading ',
+                                  style: (TextStyle(fontSize: 20)),
+                                ),
+                                Icon(Icons.open_in_new, size: 20),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
                   ],
                 ),
               ),
